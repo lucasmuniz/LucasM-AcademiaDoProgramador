3f9b688202b18e46bf50f9c6da467aa01572ba2b
LucasMuniz <lucas_goedert@hotmail.com>
2014-07-30 11:21:27 -0300

    Exercicios

--- AcademiaDoProgramador/Unidade_1/Exercícios Complementares/_01_TrianguloAsterisco.cs
+++ AcademiaDoProgramador/Unidade_1/Exercícios Complementares/_01_TrianguloAsterisco.cs
@@ -4,14 +4,27 @@ namespace Unidade_1.Exercícios_Complementares
 {
     internal class _01_TrianguloAsterisco
     {
-        public static void Main1(string[] args)
+        public static void Main(string[] args)
         {
             // Crie umprograma que imprima na tela um triângulo de “*”.
+
+            //Primeiro modo de fazer
             Console.WriteLine("    *   ");
             Console.WriteLine("   ***  ");
             Console.WriteLine("  *****  ");
             Console.WriteLine(" ******* ");
             Console.WriteLine("*********");
+            Console.ReadKey();
+
+            //Segundo modo de fazer
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
             Console.ReadKey();
         }
     }
