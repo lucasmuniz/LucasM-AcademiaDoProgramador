620cf447fbec5c4ec7a7e21ff4210b2084216d26
LucasMuniz <lucas_goedert@hotmail.com>
2014-07-30 09:51:28 -0300

    Fixacao

--- AcademiaDoProgramador/Unidade_1/Exercícios Fixação/_06_ImparesPares.cs
+++ AcademiaDoProgramador/Unidade_1/Exercícios Fixação/_06_ImparesPares.cs
@@ -4,7 +4,7 @@ namespace Unidade_1.Exercícios_Fixação
 {
     internal class _06_ImparesPares
     {
-        public static void Main(string[] args)
+        public static void Main6(string[] args)
         {
             //Faça um programa que percorra todos os números de 1 até 100. Para os números ímpares, deve ser impresso um “*”,
             //e para os números pares, deve ser impresso dois “**”.
---
+++ AcademiaDoProgramador/Unidade_1/Exercícios Fixação/_07_ImprimePi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Unidade_1.Exercícios_Fixação
+{
+    internal class _07_ImprimePI
+    {
+        public static void Main7(string[] args)
+        {   //Faça um programa que percorra todos os números de 1 até 100. Para os números múltiplos de 4,
+            //imprima a palavra “PI”, e para os outros, imprima o próprio número.
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Console.WriteLine("PI");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
