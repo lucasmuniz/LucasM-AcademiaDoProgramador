f2d31c5589994f56e6c82a8f11872e89b5c032a9
LucasMuniz <lucas_goedert@hotmail.com>
2014-07-30 09:27:42 -0300

    Primeiro Ex

---
+++ AcademiaDoProgramador/Unidade_1/Exercícios Fixação/PrimeiroPrograma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Unidade_1.Exercícios_Fixação
+{
+    internal class PrimeiroPrograma
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Academia do Programador!");
+            Console.ReadKey();
+        }
+    }
+}
