bd7327cb1edaf0495f275098b9f443e1d71598ee
LucasMuniz <lucas_goedert@hotmail.com>
2014-08-06 09:07:59 -0300

    Exerc

--- AcademiaDoProgramador/Unidade_2/Exercícios Fixação/Orientacao_a_Objetos/ContaEAgencia.cs
+++ AcademiaDoProgramador/Unidade_2/Exercícios Fixação/Orientacao_a_Objetos/ContaEAgencia.cs
@@ -12,14 +12,28 @@
         {
             ContaEAgencia c = new ContaEAgencia();
             c.Deposita(100);
+            c.ImprimeExtrato();
+            c.Saca(50);
+            c.ImprimeExtrato();
+
             double saldoDisponivel = c.ConsultaSaldoDisponivel();
-            System.Console.WriteLine(saldoDisponivel);
+            System.Console.WriteLine("Seu saldo é: {0}",saldoDisponivel);
             System.Console.ReadKey();
         }
 
         private void Deposita(double valor)
         {
             this.saldo += valor;
+        }
+
+        private void Saca(double valor)
+        {
+            this.saldo -= valor;
+        }
+
+        private void ImprimeExtrato()
+        {
+            System.Console.WriteLine("Saldo: {0}", this.saldo);
         }
 
         public double ConsultaSaldoDisponivel()
